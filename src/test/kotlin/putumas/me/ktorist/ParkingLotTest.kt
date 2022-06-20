92c40eae6aa94eea97c9c1fab6182e5638722ffd
putumas <putumas.mertayasa.e@thalesdigital.io>
2022-06-21 00:01:30 +0800

    chore: add parking lot

--- src/test/kotlin/putumas/me/ktorist/ParkingLotTest.kt
+++ src/test/kotlin/putumas/me/ktorist/ParkingLotTest.kt
@@ -6,12 +6,15 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.gson.*
 import io.ktor.server.testing.*
+import java.time.Instant
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ParkingLotTest {
     @Test
     fun testPark() = testApplication {
+        val currentTime: Long = Instant.now().toEpochMilli()
         val client = createClient {
             install(ContentNegotiation) {
                 gson()
@@ -23,11 +26,13 @@ class ParkingLotTest {
         }
         val status = resp.body<Status>()
         assertEquals(HttpStatusCode.Created, resp.status)
+
         assertEquals(
             "Vehicle VAN is parked",
             status.message,
             message = "Expecting Customer stored correctly got ${status.message}"
         )
+        assertTrue(message = "Should be later than $currentTime") { status.time > currentTime }
 
     }
 }
